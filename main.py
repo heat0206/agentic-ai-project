6ecfb27e9f7a9ba087be9c9d2a7b89be4a8ead26
Heet Mankad <heetmankad06@gmail.com>
2025-12-22 17:59:31 +0530

    Completed 5 Chapters

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 
 def main():
@@ -21,12 +23,46 @@ def main():
     #Import the genai library and use the API key to create a new instance of a Gemini client
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    # creating a debug mode 
+    parser.add_argument(
+    "--verbose",
+    action="store_true",
+    help="Enable verbose output"    
+    )
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    # we need a history of the previous conversations.
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    
+
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
     )
 
-    print(response.text)
+    #To check if tokens are generated
+    if response.usage_metadata is None:
+        raise RuntimeError(
+            "Failed API request, No tokens generated!"
+        )
+    else:
+        if args.verbose:
+            #to keep a track of number of tokens used
+            prompt_tokens = response.usage_metadata.prompt_token_count
+            response_tokens = response.usage_metadata.candidates_token_count
+            print("User prompt:",args.user_prompt)
+            print("Prompt tokens:",prompt_tokens)
+            print("Response tokens:",response_tokens)
+            print("Response:\n",response.text)        
+            
+        else:
+            print(response.text)
+    
 
 
 if __name__ == "__main__":
